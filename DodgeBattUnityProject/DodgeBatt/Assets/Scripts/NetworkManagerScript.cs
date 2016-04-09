dce08d21f7267d23c7458961dbea996460656ab1
Felix Lee <felix.lee135@gmail.com>
2016-04-09 01:46:15 -0400

    Raw networks stuff

---
+++ DodgeBattUnityProject/DodgeBatt/Assets/Scripts/NetworkManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkManagerScript : NetworkManager {
+
+    GameObject player1;
+    GameObject player2;
+    Color orange = new Color(232, 118, 0);
+    int numPlayers = 0;
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        if (numPlayers == 0)
+        {
+            player1 = player;
+            player.GetComponent<Test>().setColor(Color.blue);
+        }
+        else
+        {
+            player2 = player;
+            player.GetComponent<Test>().setColor(orange);
+        }
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+
+        numPlayers++;
+    }
+}
--- DodgeBattUnityProject/DodgeBatt/Assets/Scripts/Test.cs
+++ DodgeBattUnityProject/DodgeBatt/Assets/Scripts/Test.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class Test : MonoBehaviour {
+public class Test : NetworkBehaviour {
+
+    [SyncVar]
+    private Color myColor;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +14,22 @@ public class Test : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.FindChild("Capsule").gameObject.GetComponent<Renderer>().material.color = myColor;
+
+        if (!isLocalPlayer)
+        {
+            transform.FindChild("CenterEyeAnchor").gameObject.GetComponent<Camera>().gameObject.SetActive(false);
+            return;
+        }
+
+        float x = Input.GetAxis("Horizontal") * 0.1f;
+        float z = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, 0, z);
 	}
+
+    public void setColor(Color c)
+    {
+        myColor = c;
+    }
 }
