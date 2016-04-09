306633a86174a147d76305774b8d55e94fe2d613
Max Tatiefo <d_davy_max@yahoo.fr>
2016-04-09 17:14:38 -0400

    Game Manager object added to the scene.
    Nothing else for now....

---
+++ DodgeBattUnityProject/DodgeBatt/Assets/GameManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public Transform asteroid;
+
+	public Transform comet;
+
+	public float initForce;
+
+	public Transform particleSystem;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
