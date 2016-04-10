737153bc98c43bb95c48f1578b9cbd52cb6ad0a1
tquinn <quinn.thomasj@gmail.com>
2016-04-10 07:30:47 -0400

    added music!

---
+++ DodgeBattUnityProject/DodgeBatt/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	AudioSource fxSound;
+	public AudioClip backMusic; 
+
+	// Use this for initialization
+	void Start ()
+	{
+		// Audio Source responsavel por emitir os sons
+		fxSound = GetComponent<AudioSource> ();
+		fxSound.Play ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
