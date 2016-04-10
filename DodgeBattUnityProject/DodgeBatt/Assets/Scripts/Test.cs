11bf62589b276c706fe081541c5048f76e01c0a8
Felix Lee <felix.lee135@gmail.com>
2016-04-10 06:40:11 -0400

    Bat now follows properly regardless when not looking forward

--- DodgeBattUnityProject/DodgeBatt/Assets/Scripts/Test.cs
+++ DodgeBattUnityProject/DodgeBatt/Assets/Scripts/Test.cs
@@ -14,8 +14,6 @@ public class Test : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.FindChild("Capsule").gameObject.GetComponent<Renderer>().material.color = myColor;
-
         if (!isLocalPlayer)
         {
             transform.FindChild("CenterEyeAnchor").gameObject.GetComponent<Camera>().gameObject.SetActive(false);
